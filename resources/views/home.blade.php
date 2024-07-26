e3743ad4e924d83a4bf280eb563ced10ab6577db
ReyDios714 <isc.luisevv@gmail.com>
2024-07-26 16:17:56 -0600

    Cambio Viernes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,9 @@
 <main class="main">
             <!-- Breadcrumb -->
             <ol class="breadcrumb">
-                <li class="breadcrumb-item active"><a href="/">Sistema Tribeca Prueba de Cambio 2</a></li>
+                <li class="breadcrumb-item active"><a href="/">Sistema Tribeca</a></li>
             </ol>
             <div class="container-fluid">
-
         </main>
 
 @endsection
