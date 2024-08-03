8c039844a4afa587ad950adf53655162ad519680
ReyDios714 <isc.luisevv@gmail.com>
2024-08-02 18:24:02 -0600

    Tabla de usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,155 +10,87 @@ use DB;
 
 class UserController extends Controller
 {
-    //
     public function index(Request $request)
     {
-        //
-        if($request){
+        if ($request) {
+            $sql = trim($request->get('buscarTexto'));
+            $usuarios = DB::table('users')
+                ->join('roles', 'users.idrol', '=', 'roles.id')
+                ->select('users.id', 'users.name', 'users.email', 'users.idrol', 'roles.nombre as rol')
+                ->where('users.name', 'LIKE', '%' . $sql . '%')
+                ->orderBy('users.id', 'desc')
+                ->paginate(6);
 
-            $sql=trim($request->get('buscarTexto'));
-            $usuarios=DB::table('users')
-            ->join('roles','users.idrol','=','roles.id')
-            ->select('users.id','users.nombre','users.telefono',
-            'users.email','users.usuario','users.password',
-            'users.condicion','users.idrol','users.imagen','roles.nombre as rol')
-            ->where('users.nombre','LIKE','%'.$sql.'%')
-            ->orderBy('users.id','desc')
-            ->paginate(6);
+            /*listar los roles en ventana modal*/
+            $roles = DB::table('roles')
+                ->select('id', 'nombre', 'descripcion')
+                ->where('condicion', '=', '1')->get();
 
-             /*listar los roles en ventana modal*/
-            $roles=DB::table('roles')
-            ->select('id','nombre','descripcion')
-            ->where('condicion','=','1')->get(); 
-
-            return view('user.index',["usuarios"=>$usuarios,"roles"=>$roles,"buscarTexto"=>$sql]);
-        
-            //return $usuarios;
-        }      
-
-       
+            return view('user.index', ["usuarios" => $usuarios, "roles" => $roles, "buscarTexto" => $sql]);
+        }
     }
 
     public function store(Request $request)
     {
-        //
-       
-        $user= new User();
-        $user->nombre = $request->nombre;
-        $user->telefono = $request->telefono;
+        $user = new User();
+        $user->name = $request->name;
         $user->email = $request->email;
-        $user->usuario = $request->usuario;
-        $user->password = bcrypt( $request->password);
-        $user->condicion = '1';
-        $user->idrol = $request->id_rol;  
-          
-            //inicio registrar imagen
-            //Handle File Upload
-            if($request->hasFile('imagen')){
+        $user->password = bcrypt($request->password);
+        $user->idrol = $request->id_rol;
 
-                //Get filename with the extension
-                $filenamewithExt = $request->file('imagen')->getClientOriginalName();
-                
-                //Get just filename
-                $filename = pathinfo($filenamewithExt,PATHINFO_FILENAME);
-                
-                //Get just ext
-                $extension = $request->file('imagen')->guessClientExtension();
-                
-                //FileName to store
-                $fileNameToStore = time().'.'.$extension;
-                
-                //Upload Image
-                $path = $request->file('imagen')->storeAs('public/img/usuario',$fileNameToStore);
+        // Manejar la carga de la imagen
+        if ($request->hasFile('imagen')) {
+            $filenamewithExt = $request->file('imagen')->getClientOriginalName();
+            $filename = pathinfo($filenamewithExt, PATHINFO_FILENAME);
+            $extension = $request->file('imagen')->guessClientExtension();
+            $fileNameToStore = time() . '.' . $extension;
+            $path = $request->file('imagen')->storeAs('public/img/usuario', $fileNameToStore);
+        } else {
+            $fileNameToStore = "noimagen.jpg";
+        }
 
-            
-            } else{
+        $user->imagen = $fileNameToStore;
 
-                $fileNameToStore="noimagen.jpg";
-            }
-            
-           $user->imagen=$fileNameToStore;
-
-            //fin registrar imagen
-            $user->save();
-            return Redirect::to("user"); 
+        $user->save();
+        return Redirect::to("user");
     }
 
     public function update(Request $request)
     {
-        //
-        
-        $user= User::findOrFail($request->id_usuario);
-        $user->nombre = $request->nombre;
-        $user->telefono = $request->telefono;
+        $user = User::findOrFail($request->id_usuario);
+        $user->name = $request->name;
         $user->email = $request->email;
-        $user->usuario = $request->usuario;
         $user->password = bcrypt($request->password);
-        $user->condicion = '1';
-        $user->idrol = $request->id_rol;   
-           
-           //Editar imagen
+        $user->idrol = $request->id_rol;
 
-           if($request->hasFile('imagen')){
-
-                    /*si la imagen que subes es distinta a la que está por defecto 
-                    entonces eliminaría la imagen anterior, eso es para evitar 
-                    acumular imagenes en el servidor*/ 
-                if($user->imagen != 'noimagen.jpg'){ 
-                    Storage::delete('public/img/usuario/'.$user->imagen);
-                }
-
-                
-                    //Get filename with the extension
-                $filenamewithExt = $request->file('imagen')->getClientOriginalName();
-                
-                //Get just filename
-                $filename = pathinfo($filenamewithExt,PATHINFO_FILENAME);
-                
-                //Get just ext
-                $extension = $request->file('imagen')->guessClientExtension();
-                
-                //FileName to store
-                $fileNameToStore = time().'.'.$extension;
-                
-                //Upload Image
-                $path = $request->file('imagen')->storeAs('public/img/usuario',$fileNameToStore);
-                
-                
-                
-            } else {
-                
-                $fileNameToStore = $user->imagen; 
+        // Editar imagen
+        if ($request->hasFile('imagen')) {
+            if ($user->imagen != 'noimagen.jpg') {
+                Storage::delete('public/img/usuario/' . $user->imagen);
             }
 
-               $user->imagen=$fileNameToStore;
+            $filenamewithExt = $request->file('imagen')->getClientOriginalName();
+            $filename = pathinfo($filenamewithExt, PATHINFO_FILENAME);
+            $extension = $request->file('imagen')->guessClientExtension();
+            $fileNameToStore = time() . '.' . $extension;
+            $path = $request->file('imagen')->storeAs('public/img/usuario', $fileNameToStore);
+        } else {
+            $fileNameToStore = $user->imagen;
+        }
 
+        $user->imagen = $fileNameToStore;
 
-         //fin editar imagen
-
-          $user->save();
-          return Redirect::to("user");
+        $user->save();
+        return Redirect::to("user");
     }
-
 
     public function destroy(Request $request)
     {
-        //
-        $user= User::findOrFail($request->id_usuario);
-         
-         if($user->condicion=="1"){
+        $user = User::findOrFail($request->id_usuario);
 
-                $user->condicion= '0';
-                $user->save();
-                return Redirect::to("user");
-
-           }else{
-
-                $user->condicion= '1';
-                $user->save();
-                return Redirect::to("user");
-
-            }
+        // Alternar la condición del usuario
+        $user->idrol = $user->idrol == '1' ? '0' : '1';
+        $user->save();
+        return Redirect::to("user");
     }
-
 }
